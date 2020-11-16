108c2bb1c78324778e804e4acba33dfbc418d5f4
corym6 <cmclucas@potomacschool.org>
2020-11-16 12:17:08 -0500

    Update Grid.py

--- Grid.py
+++ Grid.py
@@ -1,10 +1,18 @@
 class Grid:
 
     def __init__(self):
-        rows, columns = (11, 11)
-        self.Grid = [['~' for i in range(columns)] for j in range(rows)]
-        for r in range(11):
-            self.changeValue(0, r, Grid.letterToNum(Grid.numToLetter(r)))
-        for c in range(11):
-            self.changeValue(c, 0, Grid.numToLetter(c))
+        self.Grid = []
+        rows = 11
+        for rows in range(1, 11):
+            self.Grid.append(["~"] * 10)
+
+    def toString(self):
+        for x in self.Grid:
+            print(x)
+
+
+
+
+
+
 
