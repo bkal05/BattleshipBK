a5cff133a5f296e1f1107a1aa19775118e11cd85
corym6 <cmclucas@potomacschool.org>
2020-11-29 18:17:27 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,10 +3,22 @@ from ComputerPlayer import ComputerPlayer
 
 print("Battleship!")
 
-print("Player one place ships now:")
+print("User place ships now:")
 human = HumanPlayer()
-computer = ComputerPlayer()
+print("Place destroyer")
+human.placeShips("D" , 2)    # letter of ship and its size
+print("Place cruiser")
+human.placeShips("C" , 3)
+print("Place submarine")
+human.placeShips("S" , 3)
+print("Place battleship")
+human.placeShips("B" , 4)
+print("Place aircraft carrier")
+human.placeShips("A" , 5)
+human.printShipGrid()
 
+print("Computer places ships now:")
+computer = ComputerPlayer()
 computer.placeShips("D" , 2)
 computer.placeShips("C" , 3)
 computer.placeShips("S" , 3)
@@ -14,25 +26,17 @@ computer.placeShips("B" , 4)
 computer.placeShips("A" , 5)
 computer.printShipGrid()
 
-human.printGuessGrid()
-human.printShipGrid()
 
-print("Place destroyer")
-human.placeShips("D" , 2)    # letter of ship and its size
-human.printShipGrid()
-print("Place cruiser")
-human.placeShips("C" , 3)
-human.printShipGrid()
-print("Place submarine")
-human.placeShips("S" , 3)
-human.printShipGrid()
-print("Place battleship")
-human.placeShips("B" , 4)
-human.printShipGrid()
-print("Place aircraft carrier")
-human.placeShips("A" , 5)
-human.printShipGrid()
+while(True):
+    human.GuessShips(computer)
+    computer.GuessShips(human)
 
 
-computer.printGuessGrid()
-computer.printShipGrid()
+
+
+
+
+
+
+
+
